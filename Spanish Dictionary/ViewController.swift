962f2e3b06c6437e6beb0604f4e0dc1326b85430
Joseph Miller <josephclaymiller@gmail.com>
2018-04-30 09:45:11 -0700

    images and code

--- Spanish Dictionary/ViewController.swift
+++ Spanish Dictionary/ViewController.swift
@@ -8,18 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    @IBOutlet weak var wordImage: UIImageView!
+    @IBOutlet weak var spanishWord: UILabel!
+    let englishWords = ["red","orange","yellow","green","blue"]
+    let spanishDictionary: [String: String] = ["red":"rojo","orange":"anaranjado","yellow":"amarillo","green":"verde","blue":"azul"]
+    // let englishWords = Array(spanishDictionary.keys)
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        showSelectedWord(word: englishWords[0])
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
     }
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return englishWords.count
+    }
+    // Picker delegate functions
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return englishWords[row].capitalized
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedWord = englishWords[row]
+        showSelectedWord(word: selectedWord)
+    }
+    // Helper Function
+    func showSelectedWord(word: String) {
+        spanishWord.text = spanishDictionary[word]?.capitalized
+        wordImage.image = UIImage(named: "\(word).png")
+    }
 
 }
 
